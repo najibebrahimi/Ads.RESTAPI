b718b04f8da46a3363ee04977872e5dd4e937628
najib ebrahimi <najib.ebrahimi144@gmail.com>
2025-01-22 15:34:13 +0100

    Add project files.

---
+++ Ads.RESTAPI/Controllers/AdsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ads.RESTAPI.Models;
+
+namespace Ads.RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdsController : ControllerBase
+    {
+        private readonly AdContext _context;
+
+        public AdsController(AdContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Ads
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ad>>> GetAds()
+        {
+            return await _context.Ads.ToListAsync();
+        }
+
+        // GET: api/Ads/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ad>> GetAd(int id)
+        {
+            var ad = await _context.Ads.FindAsync(id);
+
+            if (ad == null)
+            {
+                return NotFound();
+            }
+
+            return ad;
+        }
+
+        // PUT: api/Ads/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAd(int id, Ad ad)
+        {
+            if (id != ad.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(ad).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Ads
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Ad>> PostAd(Ad ad)
+        {
+            _context.Ads.Add(ad);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetAd), new { id = ad.Id }, ad);
+        }
+
+        // DELETE: api/Ads/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAd(int id)
+        {
+            var ad = await _context.Ads.FindAsync(id);
+            if (ad == null)
+            {
+                return NotFound();
+            }
+
+            _context.Ads.Remove(ad);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AdExists(int id)
+        {
+            return _context.Ads.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Ads.RESTAPI/Models/AdContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Ads.RESTAPI.Models;
+
+public class AdContext : DbContext
+{
+    public AdContext(DbContextOptions<AdContext> options)
+    : base(options)
+    {
+    }
+
+    public DbSet<Ad> Ads { get; set; } = null!;
+}
